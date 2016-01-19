a448a2bbb968c589e339a1c17f617d987c4e3013
whelpy <woof.timber@gmail.com>
2016-01-19 10:30:00 +0300

    Initial commit.

---
+++ WahooTcx/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WahooTcx
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileOpenButton = new System.Windows.Forms.Button();
+            this.distanceUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.distanceUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FileOpenButton
+            // 
+            this.FileOpenButton.Location = new System.Drawing.Point(12, 69);
+            this.FileOpenButton.Name = "FileOpenButton";
+            this.FileOpenButton.Size = new System.Drawing.Size(173, 24);
+            this.FileOpenButton.TabIndex = 0;
+            this.FileOpenButton.Text = "Patch TCX file";
+            this.FileOpenButton.UseVisualStyleBackColor = true;
+            this.FileOpenButton.Click += new System.EventHandler(this.FileOpenButton_Click);
+            // 
+            // distanceUpDown
+            // 
+            this.distanceUpDown.Location = new System.Drawing.Point(84, 31);
+            this.distanceUpDown.Maximum = new decimal(new int[] {
+            5000000,
+            0,
+            0,
+            0});
+            this.distanceUpDown.Name = "distanceUpDown";
+            this.distanceUpDown.Size = new System.Drawing.Size(101, 20);
+            this.distanceUpDown.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Distance (m)";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 102);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.distanceUpDown);
+            this.Controls.Add(this.FileOpenButton);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(209, 141);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(209, 141);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Wahoo TCX";
+            ((System.ComponentModel.ISupportInitialize)(this.distanceUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FileOpenButton;
+        private System.Windows.Forms.NumericUpDown distanceUpDown;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WahooTcx/MainForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace WahooTcx
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FileOpenButton_Click(object sender, EventArgs e)
+        {
+            var distanceValue = distanceUpDown.Value;
+
+            var dialog = new OpenFileDialog { Filter = "TCX (*.tcx)|*.tcx" };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var doc = new XmlDocument();
+                doc.Load(dialog.FileName);
+
+                foreach (XmlNode tcd in doc.ChildNodes)
+                {
+                    if (tcd.Name != "TrainingCenterDatabase")
+                        continue;
+
+                    foreach (XmlNode activities in tcd.ChildNodes)
+                    {
+                        if (activities.Name != "Activities")
+                            continue;
+
+                        foreach (XmlNode activity in activities.ChildNodes)
+                        {
+                            if (activity.Name != "Activity")
+                                continue;
+
+                            foreach (XmlNode lap in activity.ChildNodes)
+                            {
+                                if (lap.Name != "Lap")
+                                    continue;
+
+                                foreach (XmlNode distance in lap.ChildNodes)
+                                {
+                                    if (distance.Name != "DistanceMeters")
+                                        continue;
+
+                                    var element = distance.FirstChild;
+                                    element.Value = distanceValue.ToString("F");
+                                }
+
+                                foreach (XmlNode track in lap.ChildNodes)
+                                {
+                                    if (track.Name != "Track")
+                                        continue;
+
+                                    var pointsCount = track.ChildNodes.Count;
+                                    var curDistance = 0m;
+                                    var chunk = distanceValue/(pointsCount - 1);
+
+                                    foreach (XmlNode trackpoints in track.ChildNodes)
+                                    {
+                                        bool hasDistance = false;
+                                        foreach (XmlNode distanceNode in trackpoints.ChildNodes)
+                                        {
+                                            if (distanceNode.Name != "DistanceMeters")
+                                                continue;
+
+                                            var element = distanceNode.FirstChild;
+                                            element.Value = curDistance.ToString("F");
+                                            curDistance += chunk;
+
+                                            hasDistance = true;
+                                        }
+
+                                        if (hasDistance)
+                                            continue;
+
+                                        var node = doc.CreateNode(XmlNodeType.Element, "DistanceMeters", null);
+                                        node.InnerText = curDistance.ToString("F");
+                                        trackpoints.AppendChild(node);
+
+                                        curDistance += chunk;
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+
+                doc.Save(dialog.FileName);
+                MessageBox.Show("Done");
+            }
+        }
+    }
+}
